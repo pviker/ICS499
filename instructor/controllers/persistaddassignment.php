bdc9901edb9b9b8ed283bc20f3af3f55522267bd
Patrick Viker <pviker@me.com>
2016-04-04 19:22:14 -0500

    ...

---
+++ instructor/controllers/persistaddassignment.php
@@ -0,0 +1,34 @@
+<?php
+
+	if (!isset($_SESSION)) session_start();
+	
+	require '../../controllers/db.php';
+	
+	date_default_timezone_set("America/Chicago");
+	
+	$id = $_POST['courseID'];
+	$title = $_POST['title'];
+	$description = $_POST['description'];
+	$date = $_POST['datepicker'];
+	$points = $_POST['points'];
+	$dropboxFlag = $_POST['dropbox'];
+	
+ 	if($dropboxFlag == '1'){
+ 		$dropboxFlag = 1;
+ 	} else $dropboxFlag = 0;
+	
+	$insert = "INSERT INTO assignments (courses_id, name, description, due_date, max_points, dropbox_flag)
+				VALUES ('$id', '$title', '$description', '$date', '$points', '$dropboxFlag')";
+	
+//	echo $insert;
+
+	/// insert into plans
+ 	if(mysqli_query($conn, $insert)){
+ 		$_SESSION['msg'] = "Your reply has posted successfully!";
+ 		header("Location: ../view/content/addassignment.php");
+ 	} else {
+ 		$_SESSION['errormsg4'] = "There was an error posting your reply, please try again";
+ 		header("Location: ../view/content/addassignment.php");
+ 	}
+	
+?>
--- instructor/view/content/addassignment.php
+++ instructor/view/content/addassignment.php
@@ -36,12 +36,12 @@
 				<strong>Success!</strong> Class update posted.
 			</div>
 		<?php } unset($_SESSION['msg']);?>
-		<?php if(isset($_SESSION['errormsg2'])){?>
+		<?php if(isset($_SESSION['errormsg4'])){?>
 			<div class="alert alert-danger alert-dismissible" role="alert">
 				<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 				<strong>Oh Snap!</strong> Your update was not posted, please try again.
 			</div>
-		<?php } unset($_SESSION['errormsg2']);?>
+		<?php } unset($_SESSION['errormsg4']);?>
 		
 		<!-- FORM START -->
 		<div class="row">
@@ -51,7 +51,7 @@
 			    		<h3 class="panel-title">Add assignments for <?php include $_SERVER['DOCUMENT_ROOT'].$rootDir.'includes/getcoursenum.php';?></h3>
 			  		</div>
 				  	<div class="panel-body">
-						<form role="form" action="<?php echo $rootDir ?>/instructor/controllers/materialupdate.php" method="post" enctype="multipart/form-data">
+						<form role="form" action="<?php echo $rootDir ?>/instructor/controllers/persistaddassignment.php" method="post" enctype="multipart/form-data">
 						
 							<input type="hidden" id="courseID" name="courseID" value="<?php echo $_SESSION['selectedCourse']; ?>">
 							<div class="form-group">
@@ -70,8 +70,14 @@
 						  		<label for="description">Max points</label>
 						  		<input type="text" class="form-control <?php echo $status; ?>" id="points" name="points" placeholder="ie. 150">
 						  	</div>
+						  	<div class="checkbox">
+								<label>
+									<input type="checkbox" value="1" id="dropbox" name="dropbox">
+									Select checkbox if you want to be able to make assignment uploadable
+								</label>
+							</div>
 						 	<button type="submit" name="submit" value="submit" class="btn btn-primary btn-block" style="background-color: black; " >
-						 		Upload
+						 		Add Assignment
 						 	</button>
 						</form>
 				  	</div>
