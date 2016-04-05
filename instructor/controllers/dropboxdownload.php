66dbd3220a3fba516d01cc0990b8092d55e7fee7
pschilmoeller <pschilmoeller@me.com>
2016-04-04 19:32:20 -0500

    Download scripts

---
+++ instructor/controllers/dropboxdownload.php
@@ -0,0 +1,33 @@
+<?
+
+include ("../controllers/db.php");
+
+if(isset($_POST["btn-download"])) {
+    
+    $query = "select * from dropbox where dropbox_id = '2'";
+    
+    $result = mysqli_query($conn, $query);
+    
+    $row = mysqli_fetch_assoc($result);
+    
+    $file_content = $row["file_content"];
+    $file_size = $row["file_size"];
+    $file_type = $row["file_type"];
+    $file_name = $row["file_name"];
+    
+header("Content-length: $file_size");
+header("Content-type: $file_type");
+header("Content-Disposition: attachment; filename=$file_name");
+echo $file_content;
+
+    
+    
+}
+
+
+
+
+
+
+
+?>
