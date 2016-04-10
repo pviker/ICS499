fe718933eec8283c085ccc7b86443a3488820fd4
pschilmoeller <pschilmoeller@me.com>
2016-04-10 11:42:41 -0500

    ..

--- instructor/includes/getassignments.php
+++ instructor/includes/getassignments.php
@@ -4,8 +4,8 @@ if (!isset($_SESSION)) session_start();
 
 include '../../controllers/db.php';
 
-$assignments_query = "select assignments.assignments_id, name, file_content from assignments, dropbox 
-where assignments.assignments_id=dropbox.assignments_id and assignments.assignments_id='" . $assignments_id . "' and assignments.courses_id = '" . $_SESSION["selectedCourse"] . "' and 
+$assignments_query = "select assignments.assignments_id, name, file_content, first_name, last_name, dropbox.title, dropbox.date from assignments, dropbox, student 
+where assignments.assignments_id=dropbox.assignments_id and dropbox.student_id=student.student_id and assignments.assignments_id='" . $assignments_id . "' and assignments.courses_id = '" . $_SESSION["selectedCourse"] . "' and 
 assignments.dropbox_flag='1' and dropbox.file_content!='null'";
 
 $assignments_results = mysqli_query($conn, $assignments_query);
