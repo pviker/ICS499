088b4cd947127c27f26cd5ece132cf453ef2caa5
Patrick Viker <pviker@me.com>
2016-04-10 12:08:50 -0500

    syncing

--- instructor/view/viewstudentsubmissions.php
+++ instructor/view/viewstudentsubmissions.php
@@ -23,24 +23,48 @@
             <div class="col-sm-8">
                 <div class="panel panel-primary" style="border-color: #696053;">
                     <div class="panel-heading panel-head">
-                        <h3 class="panel-title">Student Submissions</h3>
+                        <h3 class="panel-title">Student Submissions for <?php $row1 = mysqli_fetch_assoc($assignments_results);
+                        														echo $row1['name'];
+                        														mysqli_free_result($assignments_results);
+                        												?></h3>
                     </div>
+                    <table class="table">
+					    	<thead>
+					    		<tr>
+					    			<th>Student Name</th>
+					    			<th>Student Submission Description</th>
+					    			<th>Date Submitted</th>
+					    			<th>Submission Download</th>
+					    		</tr>
+					    	</thead>	
+					    		
+				<?php 
+					include '../includes/getassignments.php';
+                                
+	                if($row_count==0){
+						echo "No submissions at this time";
+					} else{
+			            while($row = mysqli_fetch_assoc($assignments_results)) {
+							echo "<tr>".
+									 "<td>".$row['first_name']. " " . $row['last_name'] . "</td>".
+									 "<td>".$row['title']."</td>".
+									 "<td>".$row['date']."</td>".
+									 "<td><a href = '../controllers/dropboxdownload.php?assignments_id=" . $row["assignments_id"] . "'>Download</a></td>
+								 </tr>";
+						
+// 				            echo "<a href = '../controllers/dropboxdownload.php?assignments_id=" . $row["assignments_id"] . "'
+// 				            		class='list-group-item'>" . $row["name"] . "</a>";
+			            } 
+	                }
+                ?>
+					 
+                    </table>
                     <div class="panel-body">
                         <div class="list-group">
                             <!-- <a href="<?php echo $rootDir ?>/view/course.php?courseID=1" class="list-group-item">Week 1</a>
                             <a href="<?php echo $rootDir ?>/view/course.php?courseID=2" class="list-group-item">Week 2</a>
                             <a href="<?php echo $rootDir ?>/view/course.php?courseID=3" class="list-group-item">Week 3</a> -->
-                            <?php 
-                                
-                            	if($row_count==0){
-									echo "No submissions at this time";
-								} else{
-	                                while($assignments_row = mysqli_fetch_assoc($assignments_results)) {
-	                                    echo "<a href = '../controllers/dropboxdownload.php?assignments_id=" . $assignments_row["assignments_id"] . "'
-	                                             class='list-group-item'>" . $assignments_row["name"] . "</a>";
-	                                } 
-                                }
-                            ?>
+
                             
                         </div>
                     </div>
