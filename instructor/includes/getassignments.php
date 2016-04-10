1dc3d2ee27469bbee771091acb3afcb09a224d1a
pschilmoeller <pschilmoeller@me.com>
2016-04-10 11:00:21 -0500

    ..

--- instructor/includes/getassignments.php
+++ instructor/includes/getassignments.php
@@ -4,8 +4,9 @@ if (!isset($_SESSION)) session_start();
 
 include '../../controllers/db.php';
 
-$assignments_query = "select assignments_id, name from assignments where courses_id = '" . $_SESSION["selectedCourse"] . "' and 
-dropbox_flag='1'";
+$assignments_query = "select assignments.assignments_id, name, file_content from assignments, dropbox 
+where assignments.assignments_id=dropbox.assignments_id and assignments.courses_id = '" . $_SESSION["selectedCourse"] . "' and 
+assignments.dropbox_flag='1' and dropbox.file_content!='null'";
 
 $assignments_results = mysqli_query($conn, $assignments_query);
 
