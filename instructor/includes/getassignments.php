bd91f53f41043642cda2333d2f037153d7491509
Patrick Viker <pviker@me.com>
2016-04-10 11:38:25 -0500

    ...

--- instructor/includes/getassignments.php
+++ instructor/includes/getassignments.php
@@ -10,5 +10,8 @@ assignments.dropbox_flag='1' and dropbox.file_content!='null'";
 
 $assignments_results = mysqli_query($conn, $assignments_query);
 
+$row_count = mysqli_num_rows($assignments_results);
+
+
 
 ?>
--- instructor/view/viewstudentsubmissions.php
+++ instructor/view/viewstudentsubmissions.php
@@ -19,7 +19,8 @@
     <div class="container-fluid">
         
         <div class="row">
-            <div class="col-sm-6">
+        	<div class="col-sm-2"></div>
+            <div class="col-sm-8">
                 <div class="panel panel-primary" style="border-color: #696053;">
                     <div class="panel-heading panel-head">
                         <h3 class="panel-title">Student Submissions</h3>
@@ -31,27 +32,22 @@
                             <a href="<?php echo $rootDir ?>/view/course.php?courseID=3" class="list-group-item">Week 3</a> -->
                             <?php 
                                 
-                                while($assignments_row = mysqli_fetch_assoc($assignments_results)) {
-                                    echo "<a href = '../controllers/dropboxdownload.php?assignments_id=" . $assignments_row["assignments_id"] . "'
-                                             class='list-group-item'>" . $assignments_row["name"] . "</a>";
-                                } 
+                            	if($row_count==0){
+									echo "No submissions at this time";
+								} else{
+	                                while($assignments_row = mysqli_fetch_assoc($assignments_results)) {
+	                                    echo "<a href = '../controllers/dropboxdownload.php?assignments_id=" . $assignments_row["assignments_id"] . "'
+	                                             class='list-group-item'>" . $assignments_row["name"] . "</a>";
+	                                } 
+                                }
                             ?>
                             
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-6">
-                <div class="panel panel-primary" style="border-color: #696053;">
-                    <div class="panel-heading panel-head">
-                        <h3 class="panel-title">Assignments due for course: <?php echo $_SESSION['selectedCourse']; ?></h3>
-                        
-                    </div>
-                    <div class="panel-body">
-                        <p>*DB query for past 30 days for currently selected course</p>
-                    </div>
-                </div>
-            </div>
+            <div class="col-sm-2"></div>
+            
         </div>
     
     </div>
