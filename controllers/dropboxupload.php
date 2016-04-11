7948539a626f7df5d80148978a6048d17f3f1b57
Patrick Viker <pviker@me.com>
2016-04-11 18:31:13 -0500

    ...

--- controllers/dropboxupload.php
+++ controllers/dropboxupload.php
@@ -16,35 +16,35 @@ if(isset($_POST["submit"])) {
     
     if($file_size > 0 && $file_size < 2097152) {
     
-    $open_file = fopen($file_temp, "r");
-    $file_content = fread($open_file, $file_size);
-    $file_content = addslashes($file_content);
-    fclose($open_file);
-    
-    $insert_query = "insert into dropbox (assignments_id, student_id, title, file_name, file_type, file_size, file_content, date) values
-    ('" . $assignments_id . "','" . $student_id . "','" . $title . "','". $file_name . "','" . $file_type . "','" . $file_size . "','" 
-    . $file_content . "','" . $date . "')";
-    
-   if(mysqli_query($conn, $insert_query)){
-            
+	    $open_file = fopen($file_temp, "r");
+	    $file_content = fread($open_file, $file_size);
+	    $file_content = addslashes($file_content);
+	    fclose($open_file);
+	    
+	    $insert_query = "insert into dropbox (assignments_id, student_id, title, file_name, file_type, file_size, file_content, date) values
+	    ('" . $assignments_id . "','" . $student_id . "','" . $title . "','". $file_name . "','" . $file_type . "','" . $file_size . "','" 
+	    . $file_content . "','" . $date . "')";
+	    
+	   if(mysqli_query($conn, $insert_query)){
+	            
             $_SESSION['msg'] = "File uploaded successfully.";
          
             header("Location: ../view/content/assignments.php");
-            
-        } else {
-            
-            $_SESSION['errormsg3'] = "Error. File not uploaded.";
-          
-            header("Location: ../view/content/assignments.php");
-        }
-        
-    } else {
-            
-        $_SESSION['errormsg3'] = "Error. File too large or not selected.";
-          
-        header("Location: ../view/content/assignments.php");
-    }
-        
-}
+	            
+	   } else {
+	            
+	        $_SESSION['errormsg3'] = "Error. File not uploaded.";
+	          
+	        header("Location: ../view/content/assignments.php");
+	   	}
+	        
+	    } else {
+	            
+	        $_SESSION['errormsg3'] = "Error. File too large or not selected.";
+	          
+	        header("Location: ../view/content/assignments.php");
+	    }
+	        
+	}
 
 ?>
