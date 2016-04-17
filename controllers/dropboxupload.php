c65d4dfc5dd52661b5cb38870bf574cc81ef62c9
Patrick Viker <pviker@me.com>
2016-04-17 11:42:49 -0500

    BIG SYNC BRO

--- controllers/dropboxupload.php
+++ controllers/dropboxupload.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 if (!isset($_SESSION)) session_start();
 include ("db.php");
@@ -21,26 +21,34 @@ if(isset($_POST["submit"])) {
 	    $file_content = addslashes($file_content);
 	    fclose($open_file);
 	    
-	    $query = "select count(student_id) from dropbox where assignments_id=" . $assignments_id . "and student_id=" . $student_id;
+	    $query = "select count(student_id) from dropbox where assignments_id=" . $assignments_id . " and student_id=" . $student_id;
 	    
 	    $result = mysqli_query($conn, $query);
 	    
 	    if($result){
-	    	$count = mysqli_fetch_row($result);
-	    	mysql_free_result($result);
+	    	$row = mysqli_fetch_assoc($result);
+	    	
+	    	$count = $row['count(student_id)'];
 	    	
 	    	if($count > 0){
-	    		$query = "select dropbox_id where assignments_id=" . $assignments_id . "and student_id=" . $student_id;
+	    		mysqli_free_result($result);
+	    		
+	    		$query = "select dropbox_id from dropbox where assignments_id=" . $assignments_id . " and student_id=" . $student_id;
+	    		
+//	    		echo $query;
 	    		$result = mysqli_query($conn, $query);
 	    		
 	    		if($result){
-	    			$dropbox_id = mysqli_fetch_row($result);
+	    			$row = mysqli_fetch_assoc($result);
+	    			$dropbox_id = $row['dropbox_id'];
 	    		}
+	    		
+// 	    		echo "<br>DROPBOXID= " . $dropbox_id . "<br>";
 	    		
 	    		$query = "UPDATE dropbox
 	    					SET title='" . $title . "', file_name='" . $file_name . "', file_type='" . $file_type . "', file_size='" . $file_size . "',
 	    						file_content='" . $file_content . "', date='" . $date . "'
-	    					WHERE dropbox_id=" .$dropbox_id;
+	    					WHERE dropbox_id='" . $dropbox_id . "'";
 	    		
 	    	} else {
 			    $query = "insert into dropbox (assignments_id, student_id, title, file_name, file_type, file_size, file_content, date) 
@@ -49,26 +57,25 @@ if(isset($_POST["submit"])) {
 		  	}
 	    }
 	    
-	   if(mysqli_query($conn, $query)){
-	            
-            $_SESSION['msg'] = "File uploaded successfully.";
-         
-            header("Location: ../view/content/assignments.php");
-	            
-	   } else {
-	            
-	        $_SESSION['errormsg3'] = "Error. File not uploaded.";
-	          
-	        header("Location: ../view/content/assignments.php");
-	   	}
-	        
-	    } else {
-	            
-	        $_SESSION['errormsg3'] = "Error. File too large or not selected.";
-	          
-	        header("Location: ../view/content/assignments.php");
-	    }
-	        
-	}
-
+	    if(mysqli_query($conn, $query)){
+		            
+	            $_SESSION['msg'] = "File uploaded successfully.";
+	         
+	            header("Location: ../view/content/assignments.php");
+		            
+		   } else {
+		            
+		        $_SESSION['errormsg3'] = "Error. File not uploaded.";
+		          
+		        header("Location: ../view/content/assignments.php");
+		   	}
+		        
+		    } else {
+		            
+		        $_SESSION['errormsg3'] = "Error. File too large or not selected.";
+		          
+		        header("Location: ../view/content/assignments.php");
+		    }
+		        
+		}
 ?>
